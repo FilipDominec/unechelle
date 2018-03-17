6e4adbdead468df09d9421c201f59e93b413e9a1
Filip Dominec <filip.dominec@gmail.com>
2018-03-17 20:20:26 +0100

    basic info

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unechelle
-reduces an echelle spectrograph image into an optical spectrum
+Reduces an echelle spectrograph image into an optical spectrum. The image is assumed to be formed from a collimated beam, dispersed by a prism and diffracted by a blazed high-order grating. When somewhat blurred, it may look like this:
+![GitHub Logo](/images/logo.png)
+
+What we shall get is an experimental curve of spectral intensity.
+
+For convenience, the program should be able to control digital cameras with *libgphoto*.
---
+++ spectral_data/plotrc_neon-nist.csv.py
@@ -0,0 +1,22 @@
+matplotlib.rc('font', size=12, family='serif')
+#for          x,  y,  n,              param,  label,  xlabel,  ylabel,  color in \
+#         zip(xs, ys, range(len(xs)), params, labels, xlabels, ylabels, colors):
+    # x, y = x[~np.isnan(y)], y[~np.isnan(y)]        ## filter-out NaN points
+    # convol = 2**-np.linspace(-2,2,25)**2; y = np.convolve(y,convol/np.sum(convol), mode='same') ## simple smoothing
+
+ #   ax.plot(x, y, label="%s" % (label), color=color)
+    #ax.plot(x, y, label="%s" % (label.split('.dat')[0]), color=colors[c%10], ls=['-','--'][int(c/10)]) 
+for xx,yy in zip(xs[0],ys[0]):
+  ax.plot((xx,xx), (0,np.log10(yy)), lw=yy/300, alpha=.4, c='r')
+ax.set_xlabel('wavelength (nm)')
+ax.set_ylabel('log of relative intensity')
+
+plot_title = sharedlabels[-4:] ## last few labels that are shared among all curves make a perfect title
+#plot_title = sharedlabels[sharedlabels.index('LASTUNWANTEDLABEL')+1:] ## optionally, use all labels after this 
+
+ax.set_title(' '.join(plot_title)) 
+ax.legend(loc='best', prop={'size':10})
+
+#np.savetxt('output.dat', np.vstack([x,ys[0],ys[1]]).T, fmt="%.4f")
+#tosave.append('_'.join(plot_title)+'.png') ## whole graph will be saved as PNG
+#tosave.append('_'.join(plot_title)+'.pdf') ## whole graph will be saved as PDF
