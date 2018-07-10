ea3b9976b5950451defd7a2d51a7803e8e78039d
Filip Dominec <filip.dominec@gmail.com>
2018-07-10 18:02:03 +0200

    realistic physical values for x to lambda conversion

--- libgphoto-test/slider-demo/slider_demo.py
+++ libgphoto-test/slider-demo/slider_demo.py
@@ -7,7 +7,7 @@ import collections
 
 """
 Formulation of the problem:
-    * White-light collimated ray impinges (under angle α from its normal) a blazed diffraction grating (with Λ grooves/mm)
+    * White-light collimated ray impinges (under angle α from its normal) a blazed diffraction grating (with Λ groove spacing)
     * It diffracts into multiple orders (e.g. 20-30 for an echelle grating under consideration) and 
       part of the light is collected by a digital camera (inclined by angle ξ from the grating normal, which should be
       given by finding the most efficient reflection on the blazed grating facets)
@@ -17,6 +17,7 @@ Formulation of the problem:
 
 We know:
     * Grating equation:     
+                                                     λ = Λ sin β      / M
                                              sin α - sin β = Mλ/Λ,    
       where β is the angle of diffracted ray.
     * Position in the middle of the sensor X = 0.5 corresponds to β = -ξ, and more generally, 
@@ -34,13 +35,9 @@ fig.subplots_adjust(left=0.05, right=0.95, bottom=0.27, top=0.99, hspace=0)
 
 ## == static settings & built-in constants ==
 t = np.arange(0.0, 1.0, 1e-4) # x-axis for plotting
-sensor_xsize_mm     = 24
-sensor_ysize_mm     = 16
-focal_distance_mm   = 125
-grooves_permm       = 75
-spectral_lines_major_nm   = np.array([585.249, 614.306, 640.225, 703.241])
-spectral_lines_midi_nm   =  np.array([576.4674, 588.189,594.483, 607.434, 609.616, 616.359, 626.649, 633.443, 638.299, 650.653, 667.828, 671.704, 692.947, 724.517])
-spectral_lines_minor_nm   = np.array([581.932, 597.553, 603.000, 621.728, 630.479, 653.288, 659.895, 717.394, 743.890])
+spectral_lines_major   = np.array([585.249, 614.306, 640.225, 703.241]) / 1e9
+spectral_lines_midi   =  np.array([576.4674, 588.189,594.483, 607.434, 609.616, 616.359, 626.649, 633.443, 638.299, 650.653, 667.828, 671.704, 692.947, 724.517]) / 1e9
+spectral_lines_minor   = np.array([581.932, 597.553, 603.000, 621.728, 630.479, 653.288, 659.895, 717.394, 743.890]) / 1e9
 #see also http://www.astrosurf.com/buil/us/spe2/calib2/neon.dat etc.
 
 default_params = collections.OrderedDict()
@@ -48,22 +45,36 @@ default_params = collections.OrderedDict()
 ## (range from, range to, initial value)
 default_params['first_order_number']	=	   (-20,    8, 20)
 default_params['last_order_number']	=	   (-20,    20, 20)
-default_params['x_to_lambda_ofs']	=	   (-1e4,  -1000, 1e4)
-default_params['x_to_lambda_lin']	=	   (-1e4,    300, 1e4)
-#default_params['x_to_lambda_quad']	=	   (-10,      0, 10)
-default_params['lambda_to_y_ofs']	=	   (-1e1,       1/10, 1e1)
-default_params['lambda_to_y_lin']	=	   (-1e-2,    1e-2/10**2, 1e-2)
-default_params['lambda_to_y_quad']	=	   (-1e-3,    0e-1, 1e-3)
-default_params['lambda_to_y_cub']	=	   (-1e-4,    0e-1, 1e-4)
-default_params['lambda_to_y_quart']	=	   (-1e-5,   0e-12, 1e-5)
 
+default_params['Λ groove spacing (μm)']	    =	   (0,  13.333, 30)
+default_params['α incident angle (rad)']    =	   (-1,  .4, 1)
+default_params['ξ camera angle (rad)']	    =	   (-1,  .2, 1)
+default_params['F camera foc dist (mm)']    =	   (10,  150, 300)
+default_params['W camera CMOS width (mm)']  =	   (1,  24, 50)
+
+#default_params['x_to_lambda_quad']	=	   (-10,   0, 10)
+default_params['lambda_to_y_ofs']	=	   (-1,    .6, 1)
+default_params['lambda_to_y_lin']	=	   (-1,    .4, 1)
+default_params['lambda_to_y_quad']	=	   (-1,    .2, 1)
+default_params['lambda_to_y_cub']	=	   (-1,    0e-1, 1)
+default_params['lambda_to_y_quart']	=	   (-1,    0e-12, 1)
 
 ## == functions defining the geometrical transformation (x->grating, y->prism) ==
 
 def p(pname): return paramsliders[pname].val
 def x_to_lambda(xx, difrorder):
-    return -np.abs((p('x_to_lambda_ofs') + xx*p('x_to_lambda_lin'))/difrorder) ## XXX
-def lambda_to_y(lam):
+    grooved = p('Λ groove spacing (μm)')       * 1e-6
+    iangle  = p('α incident angle (rad)')
+    cangle  = p('ξ camera angle (rad)')
+    fdist   = p('F camera foc dist (mm)')      * 1e-3
+    cmosw   = p('W camera CMOS width (mm)')    * 1e-3
+    #              λ = Λ/M × [sin α - sin((0.5-X) × W / 2F  -  ξ)]
+    #print(" grooved , iangle  , cangle  , fdist   , cmosw  = ", grooved , iangle  , cangle  , fdist   , cmosw   )
+    print(xx, grooved/difrorder, (np.sin(iangle)-np.sin((.5-xx)*cmosw/2/fdist - cangle)))
+    return grooved/difrorder*(np.sin(iangle)-np.sin((.5-xx)*cmosw/2/fdist - cangle))
+
+def lambda_to_y(ll):
+    lam = ll / 500e-9 # normalisation against the typical wl.
     return p('lambda_to_y_ofs') + lam*p('lambda_to_y_lin') + lam**2*p('lambda_to_y_quad') + \
             lam**3*p('lambda_to_y_cub') + lam**4*p('lambda_to_y_quart')
 
@@ -121,16 +132,17 @@ x = np.linspace(0,1,5) # image pixel range to analyze
 lines = []
 specpts = []
 for difrorder in range(int(p('first_order_number')), int(p('last_order_number')+1)):
-    yy = lambda_to_y(x_to_lambda(x,difrorder))
-    ll = ax1.plot(x, yy, lw=1) ## , color='red'
-    lines.append(ll[0])
+    linear_lambdas  = x_to_lambda(x, difrorder)
+    linear_ys       = lambda_to_y(linear_lambdas)
 
-    spectral_lines_major_xs = np.interp(spectral_lines_major_nm, x_to_lambda(x, difrorder), x)
-    spectral_lines_major_ys = lambda_to_y(spectral_lines_major_nm)
-    print('plotting line', difrorder, 'with data x,l, y=', x, x_to_lambda(x, difrorder), yy)
-    print('  spectral_lines_major_xs =', spectral_lines_major_xs)
-    print('  spectral_lines_major_ys =', spectral_lines_major_ys)
-    specpts.append(ax1.scatter(spectral_lines_major_xs, spectral_lines_major_ys, s=5))
+    print('plotting diffr. order', difrorder, 'with data x,l,y=', x, linear_lambdas, linear_ys)
+    plotline = ax1.plot(x, linear_ys, lw=1) ## , color='red'
+    lines.append(plotline[0])
+
+    major_xs = np.interp(spectral_lines_major, linear_lambdas, x)
+    major_ys = lambda_to_y(spectral_lines_major)
+    #print('  spectral_lines:spectral_lines_major, linear_lambdas, major_xs, major_ys =', spectral_lines_major, linear_lambdas, major_xs, major_ys)
+    specpts.append(ax1.scatter(major_xs, major_ys, s=5))
     #scat.set_offsets(rain_drops['position'])
     #sc = ax1.scatter(spectral_lines_midi_nm, lambda_to_y(x_to_lambda(spectral_lines_midi_nm,   difrorder)), s=3)
 #plt.axis([0, 1, -10, 10])
