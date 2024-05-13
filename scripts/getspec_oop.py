a13411b1c377f65e4b9371922918581627e4101b
Filip Dominec <filip.dominec@gmail.com>
2024-05-13 16:04:33 +0200

    cleanup

--- scripts/getspec_oop.py
+++ scripts/getspec_oop.py
@@ -5,10 +5,10 @@
 
 import datetime
 import gphoto2 as gp
+import io
 import numpy as np
 import time
 
-name = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') # unix-convenient date format (not exactly ISO8601)
 
 
 
@@ -19,77 +19,32 @@ cfg = camera.get_config()
 
 cfg.get_child_by_name('imageformat').set_value('RAW 2')
 cfg.get_child_by_name('capturesizeclass').set_value('Full Image')
-print(cfg.get_child_by_name("shutterspeed").get_value())
-cfg.get_child_by_name('shutterspeed').set_value('2')
-print(cfg.get_child_by_name("shutterspeed").get_value())
-cfg.get_child_by_name('iso').set_value('100')
+cfg.get_child_by_name('shutterspeed').set_value('0.5') # use '1' or '0.5' or '1/10' etc. according to menu
+cfg.get_child_by_name('iso').set_value('800') # use '100', '200', '400', '800' or '1600' only for 350D
 camera.set_config(cfg)
 
-a = camera.capture_preview() 
+#name = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') # unix-convenient date format (not exactly ISO8601)
+#a.save(name + '_rawdata.cr2') # for debug - save data
+
+camera_file = gp.check_result(gp.gp_camera_capture_preview(camera))
+file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+
+camera_file = gp.check_result(gp.gp_camera_capture_preview(camera))
+file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+
+camera.exit()
 
 
-a.save(name + '_rawdata.cr2') # WORKS
-
-
-#import io
-#buf = io.BytesIO()
-#a.save(buf)
-#b_image = buf.getvalue()
-
-
-#
-#rawArray = raw.postprocess(gamma=(1,1), no_auto_bright=True, output_bps=16)
-#rawArray = np.sum(rawArray, axis=2) # merge one RGBG cell, making monochrome img
-#print(rawArray.shape, np.max(rawArray), np.mean(rawArray) )
-
-
-
-
-### start anew to make new config work - and this should not be neccessary
-#cfg.get_child_by_name('imageformat').set_value('RAW 2')
-#cfg.get_child_by_name('capturesizeclass').set_value('Full Image')
-#cfg.get_child_by_name('shutterspeed').set_value('2') #cfg.get_child_by_name('shutterspeed').set_value('1/100')
-#cfg.get_child_by_name('iso').set_value('1600')
-#camera.set_config(cfg)
-#for x in range(10):
-    #b = camera.capture_preview(); 
-    #time.sleep(.001) # if it hangs - add some 1 sec delay?
-#b.save(name + '_rawdata.cr2')
-
-
-
-
-#camera.exit()
-
-
-#my_set(name='imageformat', value='RAW')
-#my_set(name='iso', value='{}'.format(iso))
-#my_set(name='iso', value='1600') # does '3200' offer advantage over '1600' ?
-#my_set(name='shutterspeed', value='30')
-#my_set(name='shutterspeed', value='1/10')
-
-
-# does nothing:
-#cfg.get_child_by_name("shutterspeed").set_value("1/500")
-#print(cfg.get_child_by_name("shutterspeed").get_value())
-#camera.set_config(cfg)
-#cfg = camera.get_config()  # ideally, `copy(cfg0)`, but "can't pickle CameraWidget object"
-#cfg.get_child_by_name("shutterspeed").set_value("1/500")
-#camera.set_config(cfg)
-
-## --- raw file demosaic ---
+## --- raw data exctraction ---
 import rawpy
-#name = '2024-04-29_142117' # XXX debug only if camera not available
-with rawpy.imread(name + '_rawdata.cr2') as raw: # TODO use open_buffer
-    #print(raw)
-    #rgb = raw.postprocess(demosaic_algorithm=0, gamma=(1,1), no_auto_bright=True, output_bps=16)
-    #print(rgb)
-    #print(rgb.shape)
 
-    pixels = raw.raw_image_visible.copy() # note the dtype is uint16, and that numpy does not check for under/overflows 
-#import imageio
-#imageio.imsave('linear.tiff', rgb)
+#name = '2024-04-29_142117' 
+#with rawpy.imread(name + '_rawdata.cr2') as raw: # debug only if camera not available
 
+with rawpy.imread(io.BytesIO(file_data)) as raw:  # saves no data on harddrive
+    # note that the obtained dtype is uint16, ranging up to 2**12 for canon 350D, 
+    # and that numpy does not check for under/overflows 
+    pixels = raw.raw_image_visible.copy() 
 
 
 # TODO disable "manual focus drive" somehow to prevent delays & fails ?
@@ -99,11 +54,17 @@ with rawpy.imread(name + '_rawdata.cr2') as raw: # TODO use open_buffer
 ## --- interactive plotting ---
 
 import matplotlib.pyplot as plt
-minclip = 270
+
+minclip = np.min(pixels[200:-200, 200:-200])
+
 pixels = np.clip(pixels, minclip, 1000000) - minclip
 pixels = (pixels/4096.)**.3
 
 print(np.min(pixels), np.max(pixels))
 plt.imshow(pixels, clim=(0, 1), cmap='inferno') #    vmin=-0.01, vmax=1
 plt.show()
+
+
+# ==== REMARKS ==== 
+#a = camera.capture_preview()  # alternate capture command, why? 
 
